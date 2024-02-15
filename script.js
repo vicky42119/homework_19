f81f51e943be51d322d00eac495097f4c79561f7
vicky42119 <beyrv416@newschool.edu>
2024-02-15 00:33:36 -0500

    up

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// Select all nav elements
+
 const navElements = document.querySelectorAll('.nav-elements');
 
-// Add event listeners to each nav element
+
 navElements.forEach(navElement => {
-  // Add mouseenter event listener
+ 
   navElement.addEventListener('mouseenter', () => {
     const underline = navElement.querySelector('.underline');
     underline.classList.add('active');
   });
 
-  // Add mouseleave event listener
+ 
   navElement.addEventListener('mouseleave', () => {
     const underline = navElement.querySelector('.underline');
     underline.classList.remove('active');
